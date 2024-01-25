85bdd805b3ced386ca43c73c7bdc3049ed751d55
EngFahd <fahdmahmoud520@gmail.com>
2024-01-25 17:44:37 +0200

    create setup service locator

---
+++ lib/core/functions/setup_service_locator.dart
@@ -0,0 +1,23 @@
+import 'package:book_store/core/utils/api_service.dart';
+import 'package:book_store/features/home/data/data%20source/home_local_data_source.dart';
+import 'package:book_store/features/home/data/data%20source/home_remote_data_source.dart';
+import 'package:book_store/features/home/domain/repo/home_repo_implemnt.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+var getIt = GetIt.instance;
+void getItHomeImplemnt() {
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      Dio(),
+    ),
+  );
+  getIt.registerSingleton<HomeRepoImplemnt>(
+    HomeRepoImplemnt(
+      homeRemoteDataSource: HomeRemoteDataSourceImpl(
+        getIt.get<ApiService>(),
+      ),
+      homeLocalDataSource: HomeLocalDataSourceImpl(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:book_store/constanceApp.dart';
+import 'package:book_store/core/functions/setup_service_locator.dart';
 import 'package:book_store/features/home/domain/entities/book_entities.dart';
+import 'package:book_store/features/home/domain/repo/home_repo_implemnt.dart';
+import 'package:book_store/features/home/domain/use-case/featch-feature-books-use-case.dart';
+import 'package:book_store/features/home/presentaion/manger/featured_box_cubite/featured_box_cubit.dart';
 import 'package:book_store/features/home/presentaion/views/book-Detils-viwe.dart';
 import 'package:book_store/features/search-viwe/presentaion/views/Search-view.dart';
 import 'package:book_store/features/spashview/presntation/views/splashviwe.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -22,26 +27,30 @@ class BookApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      routes: {
-        BookDetilesViwe.id: (context) => const BookDetilesViwe(),
-        SearchView.id: (context) => const SearchView(),
-      },
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: kMainColor,
-          // =============== remmber this
-          textTheme: GoogleFonts.montserratAlternatesTextTheme(
-              ThemeData.dark().textTheme)),
-      home: const SplashViwe(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBoxCubit(
+            FeatchFeatureBooksUseCase(
+              getIt<HomeRepoImplemnt>(),
+            ),
+          ),
+        )
+      ],
+      child: GetMaterialApp(
+        routes: {
+          BookDetilesViwe.id: (context) => const BookDetilesViwe(),
+          SearchView.id: (context) => const SearchView(),
+        },
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kMainColor,
+            // =============== remmber this
+            textTheme: GoogleFonts.montserratAlternatesTextTheme(
+                ThemeData.dark().textTheme)),
+        home: const SplashViwe(),
+      ),
     );
   }
 }
-  // final _router = GoRouter(
-  //   routes: [
-  //     GoRoute(
-  //       path: '/',
-  //       builder: (context, state) => const SplashViwe(),
-  //     ),
-  //   ],
-  // );
+
